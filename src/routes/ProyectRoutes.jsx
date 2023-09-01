daaeb210c2f1228443693de26f8b6c601ae9c16a
Marianne Garrido <marianne.garridom@gmail.com>
2023-09-01 13:54:52 -0600

    routes implementation

--- src/routes/ProyectRoutes.jsx
+++ src/routes/ProyectRoutes.jsx
@@ -1,6 +1,18 @@
 import { Home, Menus, Order, WorkBox, ContactAndUbication } from "pages";
 import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 export const ProyectRoutes = () => {
-  return <h2>Routes</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="menu" element={<Menus />} />
+        <Route path="order" element={<Order />} />
+        <Route path="work" element={<WorkBox />} />
+        <Route path="contact" element={<ContactAndUbication />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
