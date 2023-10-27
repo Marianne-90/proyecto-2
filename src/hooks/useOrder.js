68e68b71911ca8fcc6b4ca1bce990aae0f77dbde
Marianne <marianne.garridom@gmail.com>
2023-10-27 12:20:31 -0600

    documentación de use Order

--- src/hooks/useOrder.js
+++ src/hooks/useOrder.js
@@ -1,16 +1,43 @@
 import { useContext, useState } from "react";
 import { MainContext } from "context/MainContext";
 
+
+/**
+ * Custom Hook useOrder: Gestiona una lista de pedidos en un contexto compartido.
+ *
+ * Este hook permite gestionar una lista de pedidos dentro de un contexto compartido. Proporciona
+ * funciones para agregar, reducir, eliminar elementos y calcular el total de la lista de pedidos.
+ *
+ * @returns {Object} Un objeto con funciones para interactuar con la lista de pedidos.
+ */
+
+
 export default function useOrder() {
   const { orderList, setOrderList } = useContext(MainContext);
+
+
+    /**
+   * Obtiene un arreglo de todos los elementos de pedido.
+   * @returns {Array} Un arreglo de elementos de pedido.
+   */
 
   const arrayItems = () => {
     return Object.values(orderList["items"]);
   };
 
+    /**
+   * Obtiene un arreglo de elementos extra de pedido.
+   * @returns {Array} Un arreglo de elementos extra de pedido.
+   */
+
   const arrayExtra = () => {
     return Object.values(orderList["extra"]);
   };
+
+    /**
+   * Calcula el total de la lista de pedidos sumando los precios de los elementos y los elementos extra.
+   * @returns {number} El precio total de la lista de pedidos.
+   */
 
   const handleTotal = () => {
     const itemsArray = Object.values(orderList["items"]);
@@ -26,11 +53,15 @@ export default function useOrder() {
       0
     );    
 
-
-
   const totalPriceSum = ArrayPriceSum + extraPriceSum;
     return totalPriceSum;
   };
+
+    /**
+   * Elimina un elemento de la lista de pedidos.
+   * @param {string} type - El tipo de elemento a eliminar (por ejemplo, 'items' o 'extra').
+   * @param {string} title - El título del elemento a eliminar.
+   */
 
   const handleDelete = (type, title) => {
     let orders = { ...orderList };
@@ -38,6 +69,12 @@ export default function useOrder() {
 
     setOrderList(orders);
   };
+
+    /**
+   * Reduce la cantidad de un elemento de pedido en la lista.
+   * @param {string} type - El tipo de elemento a reducir (por ejemplo, 'items' o 'extra').
+   * @param {string} title - El título del elemento a reducir.
+   */
 
   const handleReduce = (type, title) => {
     let orders = { ...orderList };
@@ -50,6 +87,13 @@ export default function useOrder() {
     }
   };
 
+    /**
+   * Agrega un elemento a la lista de pedidos.
+   * @param {string} type - El tipo de elemento a agregar (por ejemplo, 'items' o 'extra').
+   * @param {string} title - El título del elemento a agregar.
+   * @param {number} price - El precio individual del elemento a agregar.
+   */
+
   const handleAddItem = (type, title) => {
     let orders = { ...orderList };
 
@@ -59,6 +103,13 @@ export default function useOrder() {
 
     setOrderList(orders);
   };
+
+    /**
+   * Agrega un elemento a la lista de pedidos o actualiza la cantidad si ya existe.
+   * @param {string} type - El tipo de elemento a agregar (por ejemplo, 'items' o 'extra').
+   * @param {string} title - El título del elemento a agregar.
+   * @param {number} price - El precio individual del elemento a agregar.
+   */
 
   const handleAdd = (type, title, price) => {
     let orders = { ...orderList };
