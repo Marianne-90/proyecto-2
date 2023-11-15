c806335d0b4c96338f20fef79171b5f04c4cdb94
Marianne <marianne.garridom@gmail.com>
2023-11-14 19:05:38 -0600

    Términos y condiciones

--- src/pages/TermsAndPrivacy/Terms.jsx
+++ src/pages/TermsAndPrivacy/Terms.jsx
@@ -1,7 +1,71 @@
 export const Terms = () => {
   return (
-    <div className="terms">
-      <h2>Política de Privacidad de Strudel Restaurante</h2>
+    <div className="terms policy">
+      <h3>Términos y Condiciones de Uso de Strudel Restaurante</h3>
+      <p>Fecha de entrada en vigor: 14/11/2023</p>
+      <h3>1. Aceptación de los Términos y Condiciones</h3>
+      <p>
+        Al acceder y utilizar el sitio web de Strudel Restaurante, usted acepta
+        cumplir con estos Términos y Condiciones y todas las leyes y
+        regulaciones aplicables. Si no está de acuerdo con alguno de estos
+        términos, no utilice nuestro sitio web.
+      </p>
+      <h3>2. Uso del Sitio Web</h3>
+      <p>
+        Usted acepta utilizar nuestro sitio web de manera ética y conforme a
+        todas las leyes aplicables. No debe realizar actividades que puedan
+        dañar, deshabilitar o sobrecargar nuestro sitio web o interferir con el
+        uso y disfrute de otros usuarios.
+      </p>
+      <h3>3. Contenido del Usuario</h3>
+      <p>
+        Al enviar contenido a nuestro sitio web, como comentarios, mensajes o
+        formularios, usted otorga a Strudel Restaurante el derecho irrevocable y
+        gratuito de usar, reproducir, modificar, adaptar, publicar, traducir,
+        distribuir y mostrar dicho contenido en cualquier medio.
+      </p>
+      <h3>4. Propiedad Intelectual</h3>
+      <p>
+        Todo el contenido y materiales disponibles en nuestro sitio web,
+        incluidos, entre otros, logotipos, texto, gráficos, imágenes, están
+        protegidos por derechos de autor, marcas comerciales u otros derechos de
+        propiedad intelectual de Strudel Restaurante.
+      </p>
+      <h3>5. Limitación de Responsabilidad</h3>
+      <p>
+        Strudel Restaurante no se hace responsable de cualquier daño directo,
+        indirecto, incidental, especial, consecuente o punitivo que surja del
+        uso o la incapacidad de utilizar nuestro sitio web, incluso si se nos ha
+        notificado la posibilidad de dicho daño.
+      </p>
+      <h3>6. Enlaces a Sitios de Terceros</h3>
+      <p>
+        Nuestro sitio web puede contener enlaces a sitios de terceros. No nos
+        hacemos responsables del contenido o las prácticas de privacidad de
+        estos sitios y usted accede a ellos bajo su propio riesgo.
+      </p>
+      <h3>7. Modificaciones</h3>
+      <p>
+        Nos reservamos el derecho de modificar, suspender o descontinuar
+        cualquier aspecto de nuestro sitio web en cualquier momento y sin previo
+        aviso. También podemos revisar estos Términos y Condiciones en cualquier
+        momento. Al continuar utilizando el sitio web después de dichas
+        modificaciones, acepta los términos revisados.
+      </p>
+      <h3>8. Ley Aplicable</h3>
+      <p>
+        Estos Términos y Condiciones se rigen por las leyes del México y
+        cualquier disputa relacionada con estos términos estará sujeta a la
+        jurisdicción exclusiva de los tribunales de Puebla, México.
+      </p>
+      <h3>9. Contacto</h3>
+      <p>
+        Si tiene preguntas o inquietudes sobre estos Términos y Condiciones,
+        comuníquese con nosotros a través de la siguiente información de
+        contacto: Strudel Restaurante Orión norte #501 plaza Anahuac
+        strudel@gmail.com 2211576525 de teléfono] Al utilizar nuestro sitio web,
+        usted acepta y está sujeto a estos Términos y Condiciones.
+      </p>
     </div>
   );
 };
